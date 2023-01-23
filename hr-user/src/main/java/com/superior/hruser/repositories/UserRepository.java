105f0ccd777ca3e640e2dbeaf2ed34536dbcddc0
Matheus Morais <morais629@gmail.com>
2023-01-23 20:30:18 -0300

    UserRepository, UserResource and ZuulConfig

---
+++ hr-user/src/main/java/com/superior/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.superior.hruser.repositories;
+
+
+import com.superior.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
