91f12c002b16cd8f3d3e81ad212c0c66f7f3de8d
Matheus Morais <morais629@gmail.com>
2023-01-25 19:19:19 -0300

    Configuring hr-auth Project

---
+++ hr-auth/src/main/java/com/devsuperior/hrauth/HrAuthApplication.java
@@ -0,0 +1,15 @@
+package com.devsuperior.hrauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class HrAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAuthApplication.class, args);
+	}
+
+}
---
+++ hr-auth/src/test/java/com/devsuperior/hrauth/HrAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
